771732513dbadeb2253763eb382309d9b10dcad0
MayconDouglasSantana <maycon.plm.098@gmail.com>
2026-08-08 10:14:47 -0300

    exercicio02

--- Main.java
+++ Main.java
@@ -7,26 +7,15 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("Calculadora de media:\n\n");
+		System.out.println("Tabuada do numero escolhido:\n\n");
 		
-		System.out.println("Digite a primeria nota");
-		double nota1 = scanner.nextDouble();
-
-		System.out.println("Digite a segunda nota");
-		double nota2 = scanner.nextDouble();
-
-		System.out.println("Digite a terceira nota");
-		double nota3= scanner.nextDouble();
+		System.out.println("Digite o numero");
+		double numero = scanner.nextDouble();
 		
-		double media = ( nota1 + nota2 + nota3 )/3;
-		
-		if(media >= 7) {
-			System.out.println("Passou!");
-		}else if(media >= 5){
-			System.out.println("Recuperação!");
-		}else {
-			System.out.println("Reprovou!");
+		for(int i = 1; i < 11; i++) {
+			System.out.println(numero+" * "+ i +" = "+ i*numero +"\n");
 		}
+
 		
 		
 		scanner.close();
