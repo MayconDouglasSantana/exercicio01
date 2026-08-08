7bd656bef93be656a16ae73caee0796cbbc85514
MayconDouglasSantana <maycon.plm.098@gmail.com>
2026-08-08 10:06:54 -0300

    exercicio01

---
+++ Main.java
@@ -0,0 +1,36 @@
+package Main;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Calculadora de media:\n\n");
+		
+		System.out.println("Digite a primeria nota");
+		double nota1 = scanner.nextDouble();
+
+		System.out.println("Digite a segunda nota");
+		double nota2 = scanner.nextDouble();
+
+		System.out.println("Digite a terceira nota");
+		double nota3= scanner.nextDouble();
+		
+		double media = ( nota1 + nota2 + nota3 )/3;
+		
+		if(media >= 7) {
+			System.out.println("Passou!");
+		}else if(media >= 5){
+			System.out.println("Recuperação!");
+		}else {
+			System.out.println("Reprovou!");
+		}
+		
+		
+		scanner.close();
+
+	}
+
+}
